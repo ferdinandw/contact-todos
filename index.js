6f7b3f801e53dbb6d050f0ec7c11d314d9e44918
ferdinandw <fwng123@gmail.com>
2020-04-08 14:38:54 +0700

    index.js

--- index.js
+++ index.js
@@ -4,40 +4,68 @@ axios.get(URL)
     .then((response) =>{
         console.log(response);
         const listHTML = document.querySelector("#contacts>ol")
-        data = response.data;})
-    //     data.forEach(item => {
-    //         const {id, name, address, email , phone, company} = item
-    //         const itemHTML = `
-    //         Name : ${name}
-    //         <br>
-    //         Address : ${address}
-    //         <br>
-    //         Email : ${email}
-    //         <br>
-    //         Phone : ${phone}
-    //         <br>
-    //         Company : ${company}
-    //         `;
-    //         listHTML.innerHTML += itemHTML;
-    //     })
-    // })
+        data = response.data;
+        data.forEach(item => {
+            const {id, name, address, email , phone, company} = item
+            const itemHTML = `
+            Name : ${name}
+            <br>
+            Address : ${address}
+            <br>
+            Email : ${email}
+            <br>
+            Phone : ${phone}
+            <br>
+            Company : ${company}
+            `;
+            listHTML.innerHTML += itemHTML;
+        })
+    })
 
-// document.getElementById('saveContact').addEventListener('submit', function(event){
-//     event.preventDefault();
-//     const name = document.getElementsByName('name')[0].value;
-//     const address = document.getElementsByName('address')[0].value;
-//     const email = document.getElementsByName('email')[0].value;
-//     const phone = document.getElementsByName('phone')[0].value;
-//     const company = document.getElementsByName('company')[0].value;
+document.getElementById('saveContact').addEventListener('submit', function(event){
+    event.preventDefault();
+    const name = document.getElementsByName('name')[0].value;
+    const address = document.getElementsByName('address')[0].value;
+    const email = document.getElementsByName('email')[0].value;
+    const phone = document.getElementsByName('phone')[0].value;
+    const company = document.getElementsByName('company')[0].value;
 
-//     axios.post(URL, {
-//         data:{
-//             name,
-//             address,
-//             email,
-//             phone,
-//             company
-//         }
-//     })
+    axios.post(URL, {
+        data:{
+            name,
+            address,
+            email,
+            phone,
+            company
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
 
-// })
+})
+
+const hapus = id =>{
+    axios.delete(`${URL}/${id}`)
+}
+
+const ubah = id => {
+    const contact = data.find(item =>{
+        return item.id === id
+    })
+
+    if (contact){
+        const name = window.prompt('Name', contact.name);
+        const address = window.prompt('Address', contact.address);
+        const email = window.prompt('Email', contact.email);
+        const phone = window.prompt('Phone Number', contact.phone);
+        const company = window.prompt('Company Name', contact.company);
+        axios.put(`${URL}/${id}`, {
+            name,
+            address,
+            email,
+            phone,
+            company
+        })
+    }
+}
